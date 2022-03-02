6d2377b1de695937943965860b2eed54d234c03b
unknown <maxime.soufer@gmail.com>
2022-03-02 15:40:12 -0500

    finally got something happening

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,27 @@
 async function getData() {
+  let postList = document.getElementById('postlist');
   let posts = await fetch("https://jsonplaceholder.typicode.com/posts");
   let postsjson = await posts.json();
-  postlist.textContent = postsjson
 
+  for (let i = 0; i< postsjson.length; i++){
+    let newItem = document.createElement("li");
+    newItem.textContent = postsjson[i].title;
+    postList.append(newItem)
+    let newItem2 = document.createElement("li");
+    newItem2.textContent = postsjson[i].body;
+    postList.append(newItem)
+  }
+
+  
+  let authorList = document.getElementById('authorlist');
   let authors = await fetch("https://jsonplaceholder.typicode.com/users");
   let authorsjson = await authors.json();
-  authorlist.textContent = authorsjson
+
+  for (let i = 0; i< authorsjson.length; i++){
+    let newItem = document.createElement("li");
+    newItem.textContent = authorsjson[i].name;
+    authorList.append(newItem)
+  }
 }
 
 getData();
